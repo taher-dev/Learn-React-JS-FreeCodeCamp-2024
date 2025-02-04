5929ab57831845ae8108eeaeea8fabf208e33832
taher-dev <monmoyzx@gmail.com>
2025-02-04 18:15:11 +0600

    Project: Travel Journal Day-2 Commit-2

--- mini-project-travel-journal/src/App.jsx
+++ mini-project-travel-journal/src/App.jsx
@@ -4,6 +4,22 @@ import Card from "./components/Card/Card";
 import data from "./assets/js/data";
 
 const App = () => {
+  // Card List
+  const cardList = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        img={item.img}
+        title={item.title}
+        country={item.country}
+        googleMapsLink={item.googleMapsLink}
+        dates={item.dates}
+        text={item.text}
+      />
+    );
+  });
+
+  // Page Contents
   return (
     <>
       <header>
@@ -11,12 +27,7 @@ const App = () => {
         <h1>my travel journal</h1>
       </header>
 
-      <Card
-        img={{
-          src: "https://scrimba.com/links/travel-journal-japan-image-url",
-          alt: "Mount Fuji",
-        }}
-      />
+      <main>{cardList}</main>
     </>
   );
 };
