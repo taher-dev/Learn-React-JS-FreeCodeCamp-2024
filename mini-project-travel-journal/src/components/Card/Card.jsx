d15c6aa3b0d0f924249c1a29909354ea25302974
taher-dev <monmoyzx@gmail.com>
2025-02-05 20:27:10 +0600

    Project: Travel Journal Day-3 Commit-1

--- mini-project-travel-journal/src/components/Card/Card.jsx
+++ mini-project-travel-journal/src/components/Card/Card.jsx
@@ -1,17 +1,29 @@
 import "./Card.css";
+import marker from "../../assets/images/icon-marker.svg";
 const Card = (props) => {
   console.log(props);
 
   return (
-    <>
-      <div className="card">
+    <article className="card">
+      <div className="card-left">
         <img
-          className="card_main-img"
+          className="card-main-img"
           src={props.img.src}
           alt={props.img.alt}
         />
       </div>
-    </>
+
+      <div className="card-right">
+        <div className="card-right-line1">
+          <img src={marker} alt="marker icon" />
+          <p>{props.country}</p>
+          <a href={props.googleMapsLink}>View on Google Maps</a>
+        </div>
+        <h2 className="card-right-title">{props.title}</h2>
+        <p className="card-right-date">{props.dates}</p>
+        <p className="card-right-text">{props.text}</p>
+      </div>
+    </article>
   );
 };
 
