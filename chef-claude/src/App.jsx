987e4eef07ee7205bb8e690b20187c7874bcc690
taher-dev <monmoyzx@gmail.com>
2025-02-10 22:24:38 +0600

    Initial Commit

---
+++ chef-claude/src/App.jsx
@@ -0,0 +1,12 @@
+import "./assets/css/App.css";
+import chefLogo from "./assets/images/logo-chef-claude.png";
+
+const App = () => {
+  return (
+    <header>
+      <img src={chefLogo} alt="chef icon" />
+    </header>
+  );
+};
+
+export default App;
