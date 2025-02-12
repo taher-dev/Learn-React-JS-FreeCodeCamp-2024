93e18cb242dac80dc87990e2cee282a18ae5b369
taher-dev <monmoyzx@gmail.com>
2025-02-12 22:22:44 +0600

    Add: Ingredient add functionality

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,16 +1,35 @@
 const Main = () => {
+  const ingredients = [];
+  const ingredientsListItem = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  const ingredientsSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    if (newIngredient && !ingredients.includes(newIngredient)) {
+      ingredients.push(newIngredient);
+    }
+    e.currentTarget.reset();
+    console.log(ingredients);
+  };
+
   return (
     <main>
-      <form className="ingredient-form">
+      <form onSubmit={ingredientsSubmit} className="ingredient-form">
         <label htmlFor="ingredient">Ingredient</label>
         <input
           id="ingredient"
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add Ingredient</button>
       </form>
+
+      <ul>{ingredientsListItem}</ul>
     </main>
   );
 };
